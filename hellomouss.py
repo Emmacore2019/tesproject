3dba7b8b66464d2c5b92061e349177405db75bf0
Emmanuel Moussi <emmanuel.moussi@corpecca.africa>
2019-01-04 16:47:01 +0100

    test du premier commit formation

---
+++ hellomouss.py
@@ -0,0 +1,10 @@
+ Le volume dans le lecteur C s'appelle OS
+ Le num‚ro de s‚rie du volume est B2DC-775D
+
+ R‚pertoire de C:\Users\emmanuel\Documents\emmagit\projetpython
+
+04/01/2019  10:09    <DIR>          .
+04/01/2019  10:09    <DIR>          ..
+04/01/2019  10:09                 0 hellomouss.py
+               1 fichier(s)                0 octets
+               2 R‚p(s)  833ÿ738ÿ526ÿ720 octets libres
